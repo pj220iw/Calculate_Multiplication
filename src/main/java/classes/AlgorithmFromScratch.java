988de295ac127f156c3829485b4d9b2c65170626
pj220iw <peterjanda177@gmail.com>
2024-08-17 19:02:41 +0200

    Added an If statement for when 1 of the inputs is 0.

--- src/main/java/classes/AlgorithmFromScratch.java
+++ src/main/java/classes/AlgorithmFromScratch.java
@@ -11,18 +11,27 @@ public class AlgorithmFromScratch {
             int digit_x = number1.charAt(i) - '0';
             for (int j = length_y - 1; j >= 0; j--) {
                 int digit_y = number2.charAt(j) - '0';
+                //multiply x*y and add a carry
                 int sum = (digit_x * digit_y) + result[i+j+1];
+                //to know if theres a carry
                 result[i+j+1] = sum%10;
+                //if there is put it to the next position
                 result[i+j] += sum/10;
             }
         }
+        //reconstruct string
         StringBuilder strbuilder = new StringBuilder();
         for (int result_string : result){
             strbuilder.append(result_string);
         }
+        //to shift the string so it doesnt have 0s at the start
         int i = 0;
         while(i < strbuilder.length() && strbuilder.charAt(i) == '0'){
             i++;
+        }
+        //if the result are 0s
+        if (i == strbuilder.length()) {
+            return "0";
         }
         return strbuilder.substring(i);
     }
--- src/test/java/classes/AlgorithmFromScratchTest.java
+++ src/test/java/classes/AlgorithmFromScratchTest.java
@@ -8,18 +8,22 @@ class AlgorithmFromScratchTest {
 
     @Test
     void multiplyTwoNumbers() {
-        String result_alg = AlgorithmFromScratch.multiplyTwoNumbers("8941561498", "98456118965");
-        assertEquals("880351442579951609570",result_alg);
+        String result_alg = AlgorithmFromScratch.multiplyTwoNumbers("12345678901234567890", "11111111111111111111");
+        assertEquals("137174210013717420998628257899862825790",result_alg);
+        // Test numbers with zeros
+        assertEquals("0", AlgorithmFromScratch.multiplyTwoNumbers("0", "123456789"));
+        assertEquals("0", AlgorithmFromScratch.multiplyTwoNumbers("123456789", "0"));
+        assertEquals("0", AlgorithmFromScratch.multiplyTwoNumbers("0", "0"));
     }
     @Test
     void bigIntegerTest(){
-        String result_bigInt = String.valueOf(BigIntFunc.bigInt_Mult("8941561498", "98456118965"));
-        assertEquals("880351442579951609570", result_bigInt);
+        String result_bigInt = String.valueOf(BigIntFunc.bigInt_Mult("12345678901234567890", "11111111111111111111"));
+        assertEquals("137174210013717420998628257899862825790", result_bigInt);
     }
     @Test
     void Alg_vs_BigInt(){
-        String result_alg = AlgorithmFromScratch.multiplyTwoNumbers("8941561498", "98456118965");
-        String result_bigInt = String.valueOf(BigIntFunc.bigInt_Mult("8941561498", "98456118965"));
+        String result_alg = AlgorithmFromScratch.multiplyTwoNumbers("12345678901234567890", "11111111111111111111");
+        String result_bigInt = String.valueOf(BigIntFunc.bigInt_Mult("12345678901234567890", "11111111111111111111"));
         assertEquals(result_alg, result_bigInt);
     }
 
