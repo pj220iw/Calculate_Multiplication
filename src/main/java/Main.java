b7a923167c159c39c559077dad6d76a74e8d3e52
pj220iw <peterjanda177@gmail.com>
2024-08-17 21:10:13 +0200

    Added so that if there are no arguments.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,12 +3,18 @@ import static classes.BigIntFunc.bigInt_Mult;
 
 public class Main {
     public static void main(String[] args) {
-        if (args[0].equals("--alg1") && args.length == 3) {
+        if (args.length == 0){
+            System.out.println("You provided no arguments");
+            return;
+        } else if ((args[0].equals("--alg1") && args.length == 3)) {
             String result_alg1 = String.valueOf(bigInt_Mult(args[1], args[2]));
             System.out.printf(String.valueOf(result_alg1));
-            } else if (args[0].equals("--alg2") && args.length == 3) {
+        } else if (args[0].equals("--alg2") && args.length == 3) {
             String result_alg2 = multiplyTwoNumbers(args[1], args[2]);
             System.out.printf(result_alg2);
-        }else System.out.println("No arguments provided or not enough arguments");
+        }else{
+            System.out.println("No arguments provided or not enough arguments");
+            return;
+        }
     }
 }
